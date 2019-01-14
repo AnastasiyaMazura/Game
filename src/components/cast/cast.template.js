0a8c7a3896e4130b6de262427482a5dbee3ecb60
AnastasiyaMazura <mazura.anastasiya@gmail.com>
2019-01-15 02:36:53 +0300

    fix: change project

---
+++ src/components/cast/cast.template.js
@@ -0,0 +1,7 @@
+export default `
+<section id="cast" >
+<button class="spell" id="spell-reducto">Reducto (l-arrow)</button>
+<button class="spell" id="spell-expelliarmus">Expeliarmus (d-arrow)</button>
+<button class="spell" id="spell-legilimens">Legillimens (r-arrow)</button>
+</section>
+`;
